c0f0ce71fb9c535de71373c46320173d9a913a99
Amr Salhin <amr.a.salhin@gmail.com>
2026-05-12 22:12:24 +0300

    Create model class

---
+++ Model Binding/Model Class/Model Class/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model_Class.Models;
+
+namespace Model_Class.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("/{bookid?}/{isLoggedin?}")]
+        public IActionResult Index([FromQuery]int? bookId, bool? isloggedin, Book book) // deafult action method named Index conventionally
+        {
+            if (!isloggedin.HasValue || isloggedin == false )
+            {
+                return Unauthorized("User must be authenticated");
+            }
+
+            if(!bookId.HasValue)
+            {
+                return BadRequest("Book id is not supplied");
+            }
+            if (bookId <= 0)
+            {
+                return BadRequest("Book id cannot be less than or equal zero");
+            }
+
+            if(bookId > 1000)
+            {
+                return NotFound("Book id cannot be greater than 1000");
+            }
+
+
+            return Content($"Book {bookId} Book Class{book}");
+            
+        }
+    }
+}
---
+++ Model Binding/Model Class/Model Class/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Model_Class.Models
+{
+    public class Book
+    {
+        [FromRoute]
+        public int? BookId { get; set; }
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book Id{BookId}, Author {Author}";
+        }
+    }
+}
