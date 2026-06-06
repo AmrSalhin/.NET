db2583bd35494cb067efe28dd2d3be49782378a3
Amr Salhin <amr.a.salhin@gmail.com>
2026-06-07 01:03:06 +0300

    Using ViewData and ViewBag

--- Model Binding/ViewExample/ViewExample/Controllers/HomeController.cs
+++ Model Binding/ViewExample/ViewExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewExample.Models;
 
 namespace ViewExample.Controllers
 {
@@ -6,9 +7,28 @@ namespace ViewExample.Controllers
     {
         [Route("Home")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(List<Person> Person)
         {
+            /*Dummy Data to display in case no data sent in request*/
+            Person.Add(new() { Name = "john", DateOfBirth = new DateTime(2000,1,1),Gender = Gender.Male });
+            Person.Add(new() { Name = "Alexa", DateOfBirth = new DateTime(2004,5,30),Gender = Gender.Female });
+            ViewData["AppTitle"] = "ASP.net Application";
+            ViewData["Pepole"] = Person;
             return View();
+        }
+
+        [Route("Person-Details/{name}")]
+        public IActionResult Details(string name)
+        {
+            if (name == null)
+                return Content("No name Supplied");
+
+            List<Person> pepole = new List<Person>();
+            pepole.Add(new() { Name = "john", DateOfBirth = new DateTime(2000, 1, 1), Gender = Gender.Male });
+            pepole.Add(new() { Name = "Alexa", DateOfBirth = new DateTime(2004, 5, 30), Gender = Gender.Female });
+
+            Person? matchedPerson = pepole.Where(item =>  item.Name == name).FirstOrDefault();
+            return View(matchedPerson);
         }
     }
 }
