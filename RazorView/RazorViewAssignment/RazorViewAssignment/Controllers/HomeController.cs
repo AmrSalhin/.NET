f90fd99c7d4281425ddb24411750af892418cec6
Amr Salhin <amr.a.salhin@gmail.com>
2026-06-10 01:34:57 +0300

    Add Views Assignment

---
+++ RazorView/RazorViewAssignment/RazorViewAssignment/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorViewAssignment.Models;
+
+namespace RazorViewAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<CityWeather> cityWeathers = new List<CityWeather>()
+        { 
+            new CityWeather{CityUniqueCode = "LDN", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureFahrenheit = 33, CityName = "London" },
+            new CityWeather{CityUniqueCode = "NYC", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureFahrenheit = 60, CityName = "NEW YORK" },
+            new CityWeather{ CityUniqueCode = "PAR", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureFahrenheit = 82, CityName = "PARIS" }
+        };
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cityWeathers);
+        }
+
+        [Route("weather/{CityCode}")]
+        public IActionResult Details (string CityCode)
+        {
+            foreach(CityWeather cityWeather in cityWeathers)
+            {
+                if(cityWeather.CityUniqueCode == CityCode.ToUpper())
+                {
+                    return View(cityWeather);
+                }
+            }
+            return Content("Cannot find this City");
+        }
+    }
+}
---
+++ RazorView/RazorViewAssignment/RazorViewAssignment/Models/CityWeather.cs
@@ -0,0 +1,21 @@
+﻿namespace RazorViewAssignment.Models
+{
+    public class CityWeather
+    {
+        public string? CityUniqueCode { get; set; }
+        public string? CityName { get; set; }
+        public DateTime? DateAndTime { get; set; }
+        public int? TemperatureFahrenheit { get; set; }
+
+        public CityWeather() { }
+        public CityWeather(string cityUniqueCode, string cityName, DateTime dateAndTime, int temperatureFahrenheit)
+        {
+            CityUniqueCode = cityUniqueCode;
+            CityName = cityName;
+            DateAndTime = dateAndTime;
+            TemperatureFahrenheit = temperatureFahrenheit;
+        }
+
+
+    }
+}
