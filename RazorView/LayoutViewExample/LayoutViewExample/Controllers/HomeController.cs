2dea6313cb23d0c55c5dcd55837fb726ae098e52
Amr Salhin <amr.a.salhin@gmail.com>
2026-06-10 23:53:43 +0300

    Add Layout View

---
+++ RazorView/LayoutViewExample/LayoutViewExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("contact-support")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("about-company")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ RazorView/LayoutViewExample/LayoutViewExample/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("order-products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        [Route("search-products")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
